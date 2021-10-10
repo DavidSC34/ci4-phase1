1daf220bed57224bace6d698811d266bc9b4e103
DavidSC <david@wbcboxing.com>
2021-10-10 01:44:56 -0500

    crear el controller(rest version) y model

---
+++ app/Controllers/EmployeeController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class EmployeeController extends ResourceController
+{
+    //POST
+    public function createEmpployee()
+    {
+    }
+    //GET
+    public function listEmployees()
+    {
+    }
+    //GET
+    public function singleEmployee($emp_id)
+    {
+    }
+    //POST -> PUT
+    public function updateEmployee($emp_id)
+    {
+    }
+    //DELETE
+    public function deleteEmployee($emp_id)
+    {
+    }
+}
