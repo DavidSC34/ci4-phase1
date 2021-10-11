76146faaf7f1317aa0beec6ea7d1e962af0fc58d
DavidSC <david@wbcboxing.com>
2021-10-11 02:12:17 -0500

    update employee method

--- app/Controllers/EmployeeController.php
+++ app/Controllers/EmployeeController.php
@@ -150,6 +150,85 @@ class EmployeeController extends ResourceController
     //POST -> PUT
     public function updateEmployee($emp_id)
     {
+        //validation
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|valid_email'
+
+        ];
+
+        if (!$this->validate($rules)) {
+
+            //validatino error
+            $response = [
+                'status' => 500,
+                'message' => $this->validator->getErrors(),
+                'error' => true,
+                'data' =>  []
+            ];
+        } else {
+            //we have no error
+            $emp_object = new EmployeeModel();
+            $emp_data = $emp_object->find($emp_id);
+
+            if (!empty($emp_data)) {
+                //check profile photoshop
+                $file = $this->request->getFile('profile_image');
+
+                if (!empty($file)) {
+
+                    $image_name = $file->getName(); // test.png
+                    $temp = explode('.', $image_name);
+                    $new_image_name = round(microtime(true)) . '.' . end($temp); //unique name
+
+                    if ($file->move('images', $new_image_name)) {
+
+                        $update_data = [
+                            'name' => $this->request->getVar('name'),
+                            'email' => $this->request->getVar('email'),
+                            'profile_image' => '/images/' . $new_image_name
+                        ];
+                        $emp_object->update($emp_id, $update_data);
+                        $response = [
+                            'status' => 200,
+                            'message' => 'Employee data updated',
+                            'error' => false,
+                            'data' =>  []
+                        ];
+                    } else {
+                        //failed to upload image
+                        $response = [
+                            'status' => 500,
+                            'message' => 'Failed to upload image',
+                            'error' => true,
+                            'data' =>  []
+                        ];
+                    }
+                } else {
+                    //there is no fiel uploaded
+                    $update_data = [
+                        'name' => $this->request->getVar('name'),
+                        'email' => $this->request->getVar('email')
+                    ];
+                    $emp_object->update($emp_id, $update_data);
+                    $response = [
+                        'status' => 200,
+                        'message' => 'Employee data updated',
+                        'error' => false,
+                        'data' =>  []
+                    ];
+                }
+            } else {
+                //employee does not exist
+                $response = [
+                    'status' => 404,
+                    'message' => 'No Employee found',
+                    'error' => true,
+                    'data' =>  []
+                ];
+            }
+        }
+        return $this->respondCreated($response);
     }
     //DELETE
     public function deleteEmployee($emp_id)
